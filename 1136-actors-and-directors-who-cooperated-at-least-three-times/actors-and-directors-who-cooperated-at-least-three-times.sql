4652b77df421f0ef1a40ba16c502835966f27b5c
Purushrut Pandey <purushrut05@gmail.com>
2026-03-17 19:12:00 +0530

    Time: 653 ms (76.29%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1136-actors-and-directors-who-cooperated-at-least-three-times/actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -0,0 +1,7 @@
+/* Write your T-SQL query statement below */
+/* Write your T-SQL query statement below */
+
+select actor_id, director_id
+from actorDirector
+group by actor_id, director_id
+having count(*) >= 3;
