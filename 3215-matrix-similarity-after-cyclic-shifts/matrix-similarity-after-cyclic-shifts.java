518fba6117e7bd2516d55a313021a160c66c03ef
Purushrut Pandey <purushrut05@gmail.com>
2026-03-27 21:41:03 +0530

    Time: 1 ms (99.47%) | Memory: 47 MB (64.00%) - LeetSync

--- 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.java
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.java
@@ -5,7 +5,7 @@ class Solution {
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (i % 2 == 0) {
-                    if (mat[i][j] != mat[i][(j - k % n + n) % n]) {
+                    if (mat[i][j] != mat[i][(j + k % n + n) % n]) {
                         return false;
                     }
                 } else {
